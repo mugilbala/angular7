e23ac205eedf5b72616139fb562351b144b033f3
Balasubramanian <mugilbala@gmail.com>
2019-02-10 22:53:07 -0500

    First Commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('Heroes started...');
+  }
+
+  heroes = HEROES;
+
+  hero: Hero = {
+    id:1,
+    name:'Windstorm'
+  };
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id : 11, name: 'Mr.Nice'},
+    {id : 12, name: 'Naruto'},
+    {id : 13, name: 'Sasuke'},
+    {id : 14, name: 'Sakura'},
+    {id : 15, name: 'Jiraya'},
+    {id : 16, name: 'Madara'},
+    {id : 17, name: 'Sunade'},
+    {id : 18, name: 'Earth'},
+    {id : 19, name: 'Jupitar'},
+    {id : 20, name: 'Saturn'}
+];
