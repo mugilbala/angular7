9c9bead2750ef8a96da528b68bc2152a3e544928
Balasubramanian Velusamy <Balasubramanian.velusamy@cox.com>
2019-02-11 16:22:10 -0500

    Third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path : '', redirectTo: '/dashboard', pathMatch : 'full' },
+  { path : 'heroes', component : HeroesComponent },
+  { path : 'dashboard', component : DashboardComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
